46bcd6b4e03ca1bed57d2cf14377a3ccee601240
Ronit Samaddar <ronitsamaddar97@gmail.com>
2019-10-17 14:21:37 +0530

    Set up shared memory in controller.cpp

---
+++ Asg 7/controller.cpp
@@ -0,0 +1,40 @@
+//HEADER INCLUSIONS
+#include<iostream>//for cin,cout
+#include<sys/types.h>//for ftok(),shmat()
+#include<sys/ipc.h>//for ftok(),shmget()
+#include<sys/shm.h>//for shmget(),shmat()
+#include<cstdlib>//for exit()
+#include<cstdio>//for perror()
+
+//MACRO DEFINATIONS
+#define  shm_key 999//for use in shmget
+
+using namespace std;
+
+
+int main()
+{
+	int N,P;
+	cin>>N>>P;
+	
+	int size=(sizeof(int)*(N*3+2));
+	int shmid=shmget(shm_key,size,0666|IPC_CREAT);//generating id of shared memory
+	if(shmid==-1)
+	{
+		perror("Error: ");
+		exit(1);
+	}
+	int *mem=(int *)shmat(shmid,(void *)0,0);//attaching to the shared memory
+	if(*mem==-1)
+	{
+		perror("Error: ");
+		exit(1);
+	}
+	cout<<"We have a shared memory segment"<<endl;
+
+
+	
+
+}
+
+
