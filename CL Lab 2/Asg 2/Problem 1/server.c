ba59fb4582f7c284d9c2cb0a51e3e2fb44546731
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-01-09 00:55:55 +0530

    Finished Assignment 2 Problem 1

--- CL Lab 2/Asg 2/Problem 1/server.c
+++ CL Lab 2/Asg 2/Problem 1/server.c
@@ -4,11 +4,18 @@
 #include<string.h>					// strlen()
 #include<math.h>					// pow()
 #include<time.h>					// clock()
+#include<sys/socket.h>				// socket(),setsockopt(),bind()
+#include<sys/types.h>				// socket(),setsockopt(),bind()
+#include<netinet/in.h>				// sockaddr_in
+#include<unistd.h>					// fork(),read(),write(),close()
+
 
 
 //MACRO DEFINATIONS
 #define WORD_LENGTH 256
 #define MAX_OPERAND 30
+#define PORT 6000
+#define LOG_FILE "server_records.txt"
 
 
 //FUNCTION PROTOTYPES
@@ -19,27 +26,136 @@ char *eval_postfix(char *);			// function to evaluate postfix given in string an
 int main()
 {
 	
-	char *str;								//string input from client
-	clock_t start,end;						//start,end times for client request
-	double duration;						//elapsed times for client request
+	char *str;						//string input from client
+	clock_t start,end;				//start,end times for client request
+	double duration;				//elapsed times for client request
+	int sock;						//Variable for socket
+	int opt;						//option value for setsockopt
+	socklen_t optlen;				//length of options
+	struct sockaddr_in address;		//structure for socket address details
+	int addrlen;					//variable for storing structure size
+	int check;						//error check variable
+	int client_sock;				//socket of client
+	struct sockaddr_in addr_client; //address of client
+	socklen_t length_client; 		//length of client address
+	int client_index;				//index or number of client
+	int x;							//fork() variable
+	char *buffer;					//buffer string for message to send to client
+	FILE *fp;						//file pointer for log file
 
 
 
 	str=(char *)malloc(WORD_LENGTH*sizeof(char));
+	buffer=(char *)malloc(WORD_LENGTH*sizeof(char));
+	client_index=1;
 
 
-	start=clock();
-	//client request
+
+	//Socket Establishment
+	sock=socket(AF_INET,SOCK_STREAM,0);		
+									//AF_INET = for IPV4 operations
+									//SOCK_STREAM = for stream/connection based sockets
+	if(sock==-1)							
+    {
+        perror("SOCKET ERROR ");
+        exit(1);
+    }
+    printf("Socket established\n");
+
+
+    //Setting options for the socket
+    opt=1;
+	optlen=sizeof(opt);
+	setsockopt(sock,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT,&opt,optlen);
+
+	//Setting details of IP address or port for the socket
+    memset((void *)&address,0,sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);	
+    								//INADDR_ANY binds socket to all available interfaces
+    address.sin_port = htons(PORT);
+    addrlen = sizeof(address);
+
+    //Binding the socket to the given address
+    check=bind(sock,(struct sockaddr *)&address,sizeof(address));
+	if(check==-1)
+    {
+        perror("BIND ERROR");
+        exit(1);
+    }
+    printf("Socket bound to Port %d\n",PORT);
+
+
+    //Listening at the socket for any client requests
+    check=listen(sock,10);			//listen to a socket with maximum 10 pending connections
+    if(check==-1)
+    {
+	   perror("LISTEN ERROR");
+	   exit(1);
+    }
+    printf("Listening........\n");
+
+    //Opening server records file
+    fp=fopen(LOG_FILE,"w");
+
+
+	length_client = sizeof(addr_client);
 	
-	gets(str);
-	printf("%s\n",eval_postfix(str));
 	
-	//end of client request
-	end=clock();
+	while(1)
+	{
+		client_sock = accept(sock, (struct sockaddr *) &addr_client,&length_client);
+		int x=fork();
+		if(x==0)
+		{
+			//Child Process
+			break;
+		}
+		else
+		{
+			client_index++;
+		}
+	}
+	printf("Connected with client socket number %d\n",client_index);
+	while(1)
+	{
+		memset(str,'\0',WORD_LENGTH);
+		check=read(client_sock,str,WORD_LENGTH);
+		if(check==-1)
+		{
+			perror("READ ERROR");
+			exit(1);
+		}
+		if(strcmp(str,"EXIT")==0)
+		{
+			printf("Client socket number %d exits\n",client_index);
+			break;
+		}
+		//CLIENT REQUEST
+		start=clock();		
+		printf("Client socket number %d sent message : %s\n",client_index,str);
+		buffer=eval_postfix(str);
+		printf("Sending reply : %s\n",buffer);
+		write(client_sock,buffer,strlen(buffer));
+		end=clock();
+		//END OF CLIENT REQUEST
+		
+		duration=(double)(end - start)/(double)CLOCKS_PER_SEC;
+		fprintf(fp,"%d\t\t%s\t\t%s\t\t%lf sec\n",client_index,str,buffer,duration);
+		fflush(NULL);
+		
+		
+	}
+	close(client_sock);
+	
 
 
-	duration=(double)(end - start)/(double)CLOCKS_PER_SEC;
-	printf("Time required = %lf seconds\n",(double)duration);
+	
+	
+	
+
+
+	
 
 
 
