db1fd61590e91b12e3a4b486b0c5433a87473076
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-03-07 19:45:47 +0530

    Finished reducer code and got output for 1st query

--- CL Lab 2/Asg 8/A8_19CS60R01/Query1/reducer.py
+++ CL Lab 2/Asg 8/A8_19CS60R01/Query1/reducer.py
@@ -1,4 +1,17 @@
-import sys
-
+import  sys
+current_uid=""
+current_count=0
+print("USERS WHO HAVE RATED ATLEAST 10 PRODUCTS")
 for line in sys.stdin:
-	print(line)
+	line=line.strip()
+	uid=line.split()[0]
+	count=int(line.split()[1])
+	if(current_uid=="" or uid==current_uid):
+		current_count+=1
+	else:
+		if(current_count>=10):
+			print(current_uid,end=' ')
+		current_count=1
+	current_uid=uid
+if(current_count>=10):
+			print(current_uid,end=' ')
