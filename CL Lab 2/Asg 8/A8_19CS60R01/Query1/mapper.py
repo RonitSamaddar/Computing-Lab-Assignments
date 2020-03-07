56b8829f010fd727d5cf2388acd0f2b751cec6aa
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-03-07 19:27:39 +0530

    Finished mapper code for Query 1

--- CL Lab 2/Asg 8/A8_19CS60R01/Query1/mapper.py
+++ CL Lab 2/Asg 8/A8_19CS60R01/Query1/mapper.py
@@ -2,4 +2,7 @@ import gzip
 fp=gzip.open('../reviews.json.gz') 
 for line in fp: 
 	review=eval(line)
-	print(review) 
+	#print(review.keys())
+	#dict_keys(['reviewerID', 'asin', 'reviewerName', 'helpful', 'reviewText', 'overall', 'summary', 'unixReviewTime', 'reviewTime'])
+	print(review['reviewerID'],str(1))
+	
